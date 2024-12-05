9021b1943bc001793b7a6bdb94812e2f673d677b
buivandong12 <167645163+buivandong12@users.noreply.github.com>
2024-12-05 11:51:18 +0700

    update

--- app/model/DataSanPham.php
+++ app/model/DataSanPham.php
@@ -1,8 +1,31 @@
 <?php
+require_once 'dbConnect.php';
 class DataSanPham{
     //lấy dữ liệu SP
     //thêm SP
     //sửa SP
     //xóa SP
     //tìm kiếm SP
+    private $conn;
+    public function __construct()
+    {
+        $database = new Database();
+        $this->conn = $database->getConnection();
+    }
+
+    // lay danh sach tu du lieu
+    public function getAllProduct(){
+        $query = "select * from product";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //tra ve mang
+    }
+
+    //them sp moi 
+    public function addProduct($name_product,$price_product,$desc_product, $image){
+        $query = "INSERT INTO product (name_product, price_product, desc_product, image) 
+                  VALUES (:name, :price, :desc, :image)";
+    }
+
 }
--- app/model/SanPham.php
+++ app/model/SanPham.php
@@ -2,6 +2,57 @@
 class SanPham{
     //Đồng
     private $id;
-    private 
+    private $name_prodoct;
+    private $price_product;
+    private $desc_product;
+    private $image;
 
+    //phuong thuc khoi tao 
+    public function __construct($id,$name_prodoct,$price_product,$desc_product,$image)
+    {
+        $this->id = $id;
+        $this->name_prodoct = $name_prodoct;
+        $this->price_product = $name_prodoct;
+        $this->desc_product = $desc_product;
+        $this->image = $image;
+    }
+     // Getter và Setter cho id
+     public function getId() {
+        return $this->id;
+    }
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    // Getter và Setter cho name_product
+    public function getNameProduct() {
+        return $this->name_prodoct;
+    }
+    public function setNameProduct($name_product) {
+        $this->name_prodoct = $name_product;
+    }
+
+    // Getter và Setter cho price_product
+    public function getPriceProduct() {
+        return $this->price_product;
+    }
+    public function setPriceProduct($price_product) {
+        $this->price_product = $price_product;
+    }
+
+    // Getter và Setter cho desc_product
+    public function getDescProduct() {
+        return $this->desc_product;
+    }
+    public function setDescProduct($desc_product) {
+        $this->desc_product = $desc_product;
+    }
+
+    // Getter và Setter cho image
+    public function getImage() {
+        return $this->image;
+    }
+    public function setImage($image) {
+        $this->image = $image;
+    }
 }
