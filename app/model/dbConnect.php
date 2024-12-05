36155d551189aaffc08218d183649fd7a42337e5
An3112004 <an3112k4@gmail.com>
2024-12-05 11:45:37 +0700

    Anthay doi

--- app/model/dbConnect.php
+++ app/model/dbConnect.php
@@ -11,6 +11,7 @@
                 $this->conn = new PDO("mysql:host=" . $this->host .
                 ";dbname=" . $this->database_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
+                echo "ok";
             }catch(PDOException $exception){
                 echo "Database could not be connected: " . $exception->getMessage();
             }
